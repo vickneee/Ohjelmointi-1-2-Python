c5d286580f65d3231fa4d0b47ee74f81ba516247
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2024-08-20 18:29:40 +0300

    Mod02 Teht6 commit

---
+++ mod-02/Mod2-Teht6.py
@@ -0,0 +1,16 @@
+# Kirjoita ohjelma, joka arpoo ja tulostaa kaksi erilaista numerolukon koodia:
+# kolmenumeroisen koodin, jonka kukin numeromerkki on väliltä 0..9.
+# nelinumeroisen koodin, jonka kukin numeromerkki on väliltä 1..6.
+
+import random
+
+satunaisluku1 = random.randint(0, 9)
+satunaisluku2 = random.randint(0, 9)
+satunaisluku3 = random.randint(0, 9)
+print(satunaisluku1, satunaisluku2, satunaisluku3)
+
+satunaisluku4 = random.randint(0, 6)
+satunaisluku5 = random.randint(0, 6)
+satunaisluku6 = random.randint(0, 6)
+satunaisluku7 = random.randint(0, 6)
+print(satunaisluku4, satunaisluku5, satunaisluku6, satunaisluku7)
