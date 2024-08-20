90f5380bb9ce06e51beec4fca3d1e9fd47c14a99
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2024-08-20 17:00:02 +0300

    Mod02 Teht3 commit

---
+++ mod-02/Mod02-Teht3.py
@@ -0,0 +1,13 @@
+# Kirjoita ohjelma, joka kysyy suorakulmion kannan ja korkeuden. Ohjelma tulostaa suorakulmion piirin ja pinta-alan. Suorakulmion piiri tarkoittaa sen neljän sivun yhteispituutta.
+# pinta-ala = kanta * korkeus.
+# piiri = 2 * (kanta + korkeus).
+
+import math
+
+kanta = float(input("Anna suorakulmion kanta: "))
+korkeus = float(input("Anna suorakulmion korkeus: "))
+pintaala = kanta * korkeus
+piiri = 2 * (kanta + korkeus)
+
+print(f"Suorakulmion pinta-ala on {pintaala:.2f}")
+print(f"Suorakulmion piiri on {piiri:.2f}")
