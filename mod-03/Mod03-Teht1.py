803837a5ea98c277932119bddcf77ee62aba3319
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2024-08-23 11:07:24 +0300

    Mod03 Teht1 comment

--- mod-03/Mod03-Teht1.py
+++ mod-03/Mod03-Teht1.py
@@ -1,4 +1,6 @@
-# Kirjoita ohjelma, joka kysyy kalastajalta kuhan pituuden senttimetreinä. Jos kuha on alamittainen, ohjelma käskee laskea kuhan takaisin järveen ilmoittaen samalla käyttäjälle, montako senttiä alimmasta sallitusta pyyntimitasta puuttuu. Kuha on alamittainen, jos sen pituus on alle 37 cm.
+'''
+Kirjoita ohjelma, joka kysyy kalastajalta kuhan pituuden senttimetreinä. Jos kuha on alamittainen, ohjelma käskee laskea kuhan takaisin järveen ilmoittaen samalla käyttäjälle, montako senttiä alimmasta sallitusta pyyntimitasta puuttuu. Kuha on alamittainen, jos sen pituus on alle 37 cm.
+'''
 
 kuha = int(input("Anna kuhan pituus: "))
 if kuha <= 37:
@@ -6,5 +8,3 @@ if kuha <= 37:
     print(f"Kuha on {alamittainen} sentimetria lyhempi, kuin sallitua. Ole hyvä ja laskee kuha takasiin järveen.")
 else:
     print("Kuha on ok!")
-
-
