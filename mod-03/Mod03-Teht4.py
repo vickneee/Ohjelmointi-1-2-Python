cbc6ad0a334cfdb542c95467f0b65203ebddaca4
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2024-08-23 13:19:29 +0300

    Mod03 Teht4 commit

---
+++ mod-03/Mod03-Teht4.py
@@ -0,0 +1,15 @@
+'''
+Kirjoita ohjelma, joka kysyy vuosiluvun ja ilmoittaa, onko annettu vuosi karkausvuosi. Vuosi on karkausvuosi, jos se on jaollinen neljällä. Sadalla jaolliset vuodet ovat karkausvuosia vain jos ne ovat jaollisia myös neljälläsadalla.
+'''
+
+vuosiluku = int(input("Anna vuosiluku: "))
+if vuosiluku % 4 == 0:
+    if vuosiluku % 100 == 0:
+        if vuosiluku % 400 == 0:
+            print("Vuosiluku on karkausvuosi!")
+    else:
+        print("Vuosiluku on karkausvuosi!")
+else:
+    print("Vuosi ei ole karkausvuosi!")
+
+
