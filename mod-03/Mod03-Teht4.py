6bb72531f41f6a1e5f84a61d6002c43a17c5222d
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2024-08-24 13:42:47 +0300

    Mod03 Teht4 kommentin korjaus

--- mod-03/Mod03-Teht4.py
+++ mod-03/Mod03-Teht4.py
@@ -1,5 +1,10 @@
 '''
-Kirjoita ohjelma, joka kysyy vuosiluvun ja ilmoittaa, onko annettu vuosi karkausvuosi. Vuosi on karkausvuosi, jos se on jaollinen neljällä. Sadalla jaolliset vuodet ovat karkausvuosia vain jos ne ovat jaollisia myös neljälläsadalla.
+Kirjoita ohjelma, joka kysyy vuosiluvun ja ilmoittaa,
+onko annettu vuosi karkausvuosi.
+Vuosi on karkausvuosi,
+jos se on jaollinen neljällä.
+Sadalla jaolliset vuodet ovat karkausvuosia vain
+jos ne ovat jaollisia myös neljälläsadalla.
 '''
 
 vuosiluku = int(input("Anna vuosiluku: "))
