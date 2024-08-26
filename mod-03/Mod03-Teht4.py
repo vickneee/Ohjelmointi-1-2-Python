b54496b5f090179a2f3a6b209349826fa07885b4
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2024-08-26 09:16:55 +0300

    Mod03-2 Teht4 else lisätty

--- mod-03/Mod03-Teht4.py
+++ mod-03/Mod03-Teht4.py
@@ -12,6 +12,8 @@ if vuosiluku % 4 == 0:
     if vuosiluku % 100 == 0:
         if vuosiluku % 400 == 0:
             print("Vuosiluku on karkausvuosi!")
+        else:
+            print("Vuosi ei ole karkausvuosi!")
     else:
         print("Vuosiluku on karkausvuosi!")
 else:
