5084aaed029975929bd77800a66030fa97df64c6
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2024-08-27 13:16:28 +0300

    Mod03 Teht1 korjaus

--- mod-03/Mod03-Teht1.py
+++ mod-03/Mod03-Teht1.py
@@ -9,7 +9,7 @@ Kuha on alamittainen, jos sen pituus on alle 37 cm.
 '''
 
 kuha = float(input("Anna kuhan pituus: "))
-if kuha <= 37:
+if kuha < 37:
     alamittainen = 37 - kuha
     print(f"Kuha on {alamittainen} sentimetria lyhempi, kuin sallitua. Ole hyvä ja laskee kuha takasiin järveen.")
 else:
