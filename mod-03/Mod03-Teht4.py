24f2e061556463bcb55d040cbd018ee2623d0c6b
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2024-08-27 13:55:40 +0300

    Mod03 Teht4 vuosiluku ehtojen korjaus

--- mod-03/Mod03-Teht4.py
+++ mod-03/Mod03-Teht4.py
@@ -8,15 +8,14 @@ jos ne ovat jaollisia myös neljälläsadalla.
 '''
 
 vuosiluku = int(input("Anna vuosiluku: "))
-if vuosiluku % 4 == 0:
-    if vuosiluku % 100 == 0:
-        if vuosiluku % 400 == 0:
-            print("Vuosiluku on karkausvuosi!")
-        else:
-            print("Vuosi ei ole karkausvuosi!")
+
+if vuosiluku % 100 == 0:
+    if vuosiluku % 400 == 0:
+        print("Vuosi on karkausvuosi.")
     else:
-        print("Vuosiluku on karkausvuosi!")
+        print("Vuosi ei ole karkausvuosi.")
+
+elif vuosiluku % 4 == 0:
+    print("Vuosi on karkausvuosi.")
 else:
-    print("Vuosi ei ole karkausvuosi!")
-
-
+    print("Vuosi ei ole karkausvuosi.")
