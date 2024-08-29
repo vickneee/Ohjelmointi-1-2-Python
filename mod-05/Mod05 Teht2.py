759d051d72b018551fdf6de923a94750d8e03d3f
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2024-08-29 20:24:03 +0300

    Tehtävä 2 initial commit

---
+++ mod-05/Mod05 Teht2.py
@@ -0,0 +1,22 @@
+'''
+Kirjoita ohjelma, joka kysyy käyttäjältä lukuja siihen saakka, kunnes tämä syöttää tyhjän merkkijonon lopetusmerkiksi. Lopuksi ohjelma tulostaa saaduista luvuista viisi suurinta suuruusjärjestyksessä suurimmasta alkaen. Vihje: listan alkioiden lajittelujärjestyksen voi kääntää antamalla sort-metodille argumentiksi reverse=True.
+'''
+
+luvut = []
+luku = input("Anna luku: (Tyhjä merkkijono lopeta) ")
+
+while luku != "":
+    if luku in luvut:
+        print("Se luku on jo listalla. Dublikaatti!")
+    else:
+        luvut.append(int(luku))  # Muutetaan luku kokonaisluvuksi ennen listaan lisämistä
+        print(luvut)
+    luku = input("Anna luku: (Tyhjä merkkijono lopeta) ")
+
+luvut.sort(reverse=True)
+print(f"Viisi suurinta luku suuruusjärjestyksessä suurimmasta alkaen {luvut[0:5]}")
+
+
+
+
+
