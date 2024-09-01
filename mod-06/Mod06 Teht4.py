4ef7373d714de288e4bfded186a24cfbda2d99ee
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2024-09-01 23:36:18 +0300

    Initial commit

--- mod-06/Mod06 Teht4.py
+++ mod-06/Mod06 Teht4.py
@@ -1,3 +1,11 @@
 '''
 Kirjoita funktio, joka saa parametrinaan listan kokonaislukuja. Ohjelma palauttaa listassa olevien lukujen summan. Kirjoita testausta varten pääohjelma, jossa luot listan, kutsut funktiota ja tulostat sen palauttaman summan.
 '''
+
+
+def summalista(parameterlista):
+    return sum(parameterlista)
+
+
+lista = [1, 2, 9, 8, 3, 4, 5]
+print(f"Lista tulostetuna on: {lista}. Listassa olevien lukujen summa on: {summalista(lista)}.")
