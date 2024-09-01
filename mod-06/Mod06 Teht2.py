681f286860c33d1a87d815069730377183d3c944
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2024-09-01 22:41:44 +0300

    Initial commit

--- mod-06/Mod06 Teht2.py
+++ mod-06/Mod06 Teht2.py
@@ -1,3 +1,20 @@
 '''
 Muokkaa edellistä funktiota siten, että funktio saa parametrinaan nopan tahkojen yhteismäärän. Muokatun funktion avulla voit heitellä esimerkiksi 21-tahkoista roolipelinoppaa. Edellisestä tehtävästä poiketen nopan heittelyä jatketaan pääohjelmassa kunnes saadaan nopan maksimisilmäluku, joka kysytään käyttäjältä ohjelman suorituksen alussa.
 '''
+
+import random
+
+
+def satunnainennoppa():
+    arvottu = random.randint(1, 6)
+    return arvottu
+
+
+nopan_silmaluku = int(input("Anna nopan silmäluku: "))
+luku = satunnainennoppa()
+laske = luku
+while laske <= nopan_silmaluku:
+    print(luku)
+    laske += luku
+    luku = satunnainennoppa()
+print(f"Kokonaissumma on {laske}! Nopan silmaluku oli {nopan_silmaluku}. Ohjelma lopetetaan.")
