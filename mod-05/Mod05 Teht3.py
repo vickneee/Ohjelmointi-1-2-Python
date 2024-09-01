1d40b750489bbaad2f40f0172c2651a879bbe46d
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2024-09-02 00:13:51 +0300

    Kommentin rivien korjaus

--- mod-05/Mod05 Teht3.py
+++ mod-05/Mod05 Teht3.py
@@ -1,10 +1,13 @@
-'''
-Kirjoita ohjelma, joka kysyy käyttäjältä kokonaisluvun ja ilmoittaa, onko se alkuluku. Tässä tehtävässä alkulukuja
+"""
+Kirjoita ohjelma, joka kysyy käyttäjältä kokonaisluvun
+ja ilmoittaa, onko se alkuluku. Tässä tehtävässä alkulukuja
 ovat luvut, jotka ovat jaollisia vain ykkösellä ja itsellään.
 
-Esimerkiksi luku 13 on alkuluku, koska se voidaan jakaa vain luvuilla 1 ja 13 siten, että jako menee tasan.
-Toisaalta esimerkiksi luku 21 ei ole alkuluku, koska se voidaan jakaa tasan myös luvulla 3 tai luvulla 7.
-'''
+Esimerkiksi luku 13 on alkuluku,
+koska se voidaan jakaa vain luvuilla 1 ja 13 siten, että jako menee tasan.
+Toisaalta esimerkiksi luku 21 ei ole alkuluku,
+koska se voidaan jakaa tasan myös luvulla 3 tai luvulla 7.
+"""
 
 # negatiiviset numerot, 0 ja 1 eivat ole alkulukuja
 numero = int(input("Anna numero: "))
