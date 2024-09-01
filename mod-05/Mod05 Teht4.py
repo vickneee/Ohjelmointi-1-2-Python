e60b4e0de3bb87b16d301a7669bf2e0f492e1781
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2024-09-02 00:14:39 +0300

    Kommentin rivien korjaus

--- mod-05/Mod05 Teht4.py
+++ mod-05/Mod05 Teht4.py
@@ -1,6 +1,13 @@
-'''
-Kirjoita ohjelma, joka kysyy käyttäjältä viiden kaupungin nimet yksi kerrallaan (käytä for-toistorakennetta nimien kysymiseen) ja tallentaa ne listarakenteeseen. Lopuksi ohjelma tulostaa kaupunkien nimet yksi kerrallaan allekkain samassa järjestyksessä kuin ne syötettiin. käytä for-toistorakennetta nimien kysymiseen ja for/in toistorakennetta niiden läpikäymiseen.
-'''
+"""
+Kirjoita ohjelma,
+joka kysyy käyttäjältä viiden kaupungin nimet yksi kerrallaan
+(käytä for-toistorakennetta nimien kysymiseen)
+ja tallentaa ne listarakenteeseen.
+Lopuksi ohjelma tulostaa kaupunkien nimet
+yksi kerrallaan allekkain samassa järjestyksessä kuin ne syötettiin.
+Käytä for-toistorakennetta nimien kysymiseen
+ja for/in toistorakennetta niiden läpikäymiseen.
+"""
 
 kaupungit = []
 
