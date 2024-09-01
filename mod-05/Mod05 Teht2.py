88ffeb54aed42b42fc505a4424a09fe6b224a8f7
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2024-09-02 00:13:17 +0300

    Kommentin rivien korjaus

--- mod-05/Mod05 Teht2.py
+++ mod-05/Mod05 Teht2.py
@@ -1,6 +1,11 @@
-'''
-Kirjoita ohjelma, joka kysyy käyttäjältä lukuja siihen saakka, kunnes tämä syöttää tyhjän merkkijonon lopetusmerkiksi. Lopuksi ohjelma tulostaa saaduista luvuista viisi suurinta suuruusjärjestyksessä suurimmasta alkaen. Vihje: listan alkioiden lajittelujärjestyksen voi kääntää antamalla sort-metodille argumentiksi reverse=True.
-'''
+"""
+Kirjoita ohjelma, joka kysyy käyttäjältä lukuja siihen saakka,
+kunnes tämä syöttää tyhjän merkkijonon lopetusmerkiksi.
+Lopuksi ohjelma tulostaa saaduista luvuista viisi suurinta
+suuruusjärjestyksessä suurimmasta alkaen.
+Vihje: listan alkioiden lajittelujärjestyksen voi kääntää
+antamalla sort-metodille argumentiksi reverse=True.
+"""
 
 luvut = []
 luku = input("Anna luku: (Tyhjä merkkijono lopeta) ")
