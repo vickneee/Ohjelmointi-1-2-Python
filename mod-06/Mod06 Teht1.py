07b6be2a99cc50bee102d6002218767386ec49d2
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2024-09-02 00:16:01 +0300

    Kommentin rivien korjaus

--- mod-06/Mod06 Teht1.py
+++ mod-06/Mod06 Teht1.py
@@ -1,6 +1,9 @@
-'''
-Kirjoita parametriton funktio, joka palauttaa paluuarvonaan satunnaisen nopan silmäluvun väliltä 1..6. Kirjoita pääohjelma, joka heittää noppaa niin kauan kunnes tulee kuutonen. Pääohjelma tulostaa kunkin heiton jälkeen saadun silmäluvun.
-'''
+"""
+Kirjoita parametriton funktio,
+joka palauttaa paluuarvonaan satunnaisen nopan silmäluvun väliltä 1..6.
+Kirjoita pääohjelma, joka heittää noppaa niin kauan kunnes tulee kuutonen. Pääohjelma tulostaa kunkin heiton jälkeen saadun
+silmäluvun.
+"""
 
 import random
 
