d48c7df8cdd4096cfd12ec5e031ea1831c1bbef5
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2024-09-02 11:16:39 +0300

    Initial commit

---
+++ mod-07/Mod07 Teht1.py
@@ -0,0 +1,7 @@
+"""
+Kirjoita ohjelma, joka kysyy käyttäjältä kuukauden numeron,
+jonka jälkeen ohjelma tulostaa sitä vastaavan vuodenajan (kevät, kesä, syksy, talvi).
+Tallenna ohjelmassasi kuukausia vastaavat vuodenajat merkkijonoina monikkotietorakenteeseen.
+Määritellään kukin vuodenaika kolmen kuukauden mittaiseksi siten,
+että joulukuu on ensimmäinen talvikuukausi.
+"""
