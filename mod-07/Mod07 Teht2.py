443ce65e46ef41f936149326938fff9fa0c682d8
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2024-09-03 14:06:56 +0300

    Initial commit

--- mod-07/Mod07 Teht2.py
+++ mod-07/Mod07 Teht2.py
@@ -6,3 +6,19 @@ tai Aiemmin syötetty nimi sen mukaan, syötettiinkö nimi ensimmäistä kertaa.
 Lopuksi ohjelma luettelee syötetyt nimet yksi kerrallaan allekkain mielivaltaisessa järjestyksessä.
 Käytä joukkotietorakennetta nimien tallentamiseen.
 """
+
+# Alustetaan tyhjä joukku
+nimi_joukko = set()
+
+nimi = input("Anna nimi (Tyhjä merkkijono lopeta): ")
+
+while nimi != "":
+    if nimi in nimi_joukko:
+        print("Aiemmin syötetty nimi")
+    else:
+        print("Ilmeistyi uusi nimi. Lisätään nimi joukkoon.")
+        nimi_joukko.add(nimi)  # Joukkotietorakennetta nimien tallentamiseen.
+    nimi = input("Anna nimi (Tyhjä merkkijono lopeta): ")
+
+for i in nimi_joukko:
+    print(i)
