14c4dae8bbeb086d3cdd637e10013105c87b7672
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2024-09-06 09:39:25 +0300

    Parametri lisätty funktioon

--- mod-06/Mod06 Teht2.py
+++ mod-06/Mod06 Teht2.py
@@ -1,20 +1,28 @@
-'''
-Muokkaa edellistä funktiota siten, että funktio saa parametrinaan nopan tahkojen yhteismäärän. Muokatun funktion avulla voit heitellä esimerkiksi 21-tahkoista roolipelinoppaa. Edellisestä tehtävästä poiketen nopan heittelyä jatketaan pääohjelmassa kunnes saadaan nopan maksimisilmäluku, joka kysytään käyttäjältä ohjelman suorituksen alussa.
-'''
+"""
+Muokkaa edellistä funktiota siten,
+että funktio saa parametrinaan nopan tahkojen yhteismäärän.
+Muokatun funktion avulla voit heitellä esimerkiksi 21-tahkoista roolipelinoppaa.
+Edellisestä tehtävästä poiketen nopan heittelyä jatketaan pääohjelmassa
+kunnes saadaan nopan maksimisilmäluku,
+joka kysytään käyttäjältä ohjelman suorituksen alussa.
+"""
 
 import random
 
 
-def satunnainennoppa():
-    arvottu = random.randint(1, 6)
-    return arvottu
+def satunnainennoppa(tahkoja):
+    satunainnen = random.randint(1, tahkoja)
+    return satunainnen
 
 
 nopan_silmaluku = int(input("Anna nopan silmäluku: "))
-luku = satunnainennoppa()
-laske = luku
-while laske <= nopan_silmaluku:
-    print(luku)
-    laske += luku
-    luku = satunnainennoppa()
-print(f"Kokonaissumma on {laske}! Nopan silmaluku oli {nopan_silmaluku}. Ohjelma lopetetaan.")
+
+while True:
+    arvottu = satunnainennoppa(nopan_silmaluku)
+    print(arvottu)
+
+    if arvottu == nopan_silmaluku:
+        break
+
+
+print(f"Kokonaissumma on {arvottu}! Nopan silmaluku oli {nopan_silmaluku}. Ohjelma lopetetaan.")
