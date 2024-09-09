c3c892e62671fa87cb70db5896bafd122f21c508
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2024-09-09 10:20:42 +0300

    Versio 2 lisätty

--- mod-07/Mod07 Teht1.py
+++ mod-07/Mod07 Teht1.py
@@ -6,7 +6,23 @@ Määritellään kukin vuodenaika kolmen kuukauden mittaiseksi siten,
 että joulukuu on ensimmäinen talvikuukausi.
 """
 
+# Version 1
 kuukaudet = ("Talvi", "Talvi", "Kevät", "Kevät", "Kevät", "Kesä", "Kesä", "Kesä", "Syksy", "Syksy", "Syksy", "Talvi")
 numero = int(input("Anna kuukauden numero: "))
-vuodenaika = kuukaudet[numero-1]
+vuodenaika = kuukaudet[numero - 1]
 print(vuodenaika)
+
+# Version 2
+vuodenaika = ("Talvi", "Kevät", "Kesä", "Syksy")
+kuukausi = int(input("Anna kuukauden numero: "))
+
+if 3 <= kuukausi <= 5:
+    print(vuodenaika[1])
+elif 6 <= kuukausi <= 8:
+    print(vuodenaika[2])
+elif 9 <= kuukausi <= 11:
+    print(vuodenaika[3])
+elif kuukausi == 12 or kuukausi <= 2:
+    print(vuodenaika[0])
+else:
+    print("Virheellinen kuukauden numero")
