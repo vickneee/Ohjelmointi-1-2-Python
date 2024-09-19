4afaff2f548b39e8f8250bde8f43a314ba3b2b2d
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2024-09-19 23:08:08 +0300

    Tehtävän uudelleen tekeminen

---
+++ mod-06/Mod06-Teht1.py
@@ -0,0 +1,23 @@
+"""
+Kirjoita parametriton funktio,
+joka palauttaa paluuarvonaan satunnaisen nopan silmäluvun väliltä 1..6.
+Kirjoita pääohjelma, joka heittää noppaa niin kauan kunnes tulee kuutonen. Pääohjelma tulostaa kunkin heiton jälkeen saadun
+silmäluvun.
+"""
+
+import random
+
+
+def satunainen_noppa():
+    satunainennop = random.randint(1, 6)
+    return satunainennop
+
+
+luku = satunainen_noppa()
+laske = 1
+while luku != 6:
+    print(luku)
+    luku = satunainen_noppa()
+    laske += 1
+
+print(f"Ohjelma lopetetaan, koska kierokksella {laske} tuli kuutonen!")
