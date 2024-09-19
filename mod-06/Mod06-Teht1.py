a8ed038835f796afd0b5fcfcea4263bde2b961e2
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2024-09-19 23:08:25 +0300

    Tehtävän uudelleen tekeminen

--- mod-06/Mod06-Teht1.py
+++ mod-06/Mod06-Teht1.py
@@ -1,7 +1,8 @@
 """
 Kirjoita parametriton funktio,
 joka palauttaa paluuarvonaan satunnaisen nopan silmäluvun väliltä 1..6.
-Kirjoita pääohjelma, joka heittää noppaa niin kauan kunnes tulee kuutonen. Pääohjelma tulostaa kunkin heiton jälkeen saadun
+Kirjoita pääohjelma, joka heittää noppaa niin kauan kunnes tulee kuutonen.
+Pääohjelma tulostaa kunkin heiton jälkeen saadun
 silmäluvun.
 """
 
