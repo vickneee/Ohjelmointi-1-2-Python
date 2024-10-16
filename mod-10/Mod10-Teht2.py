8cfa007ace84829a29bf3f6783ab7a09df1aa345
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2024-10-16 20:16:38 +0300

    Mod10 Teht2 commit

--- mod-10/Mod10-Teht2.py
+++ mod-10/Mod10-Teht2.py
@@ -10,22 +10,6 @@ class Hissi:
         self.alin_kerros = alin_kerros
         self.ylin_kerros = ylin_kerros
 
-    # Metodi hissin siirtämiseksi ylöspäin (functio)
-    def kerros_ylos(self):
-        if self.alin_kerros < self.ylin_kerros:
-            self.alin_kerros += 1
-            print(f"Hissi on kerroksessa {self.alin_kerros}")
-        else:
-            print("Hissi on jo ylimmässä kerroksessa.")
-
-    # Metodi hissin siirtämiseksi alaspäin (functio)
-    def kerros_alas(self):
-        if self.alin_kerros > 1:
-            self.alin_kerros -= 1
-            print(f"Hissi on kerroksessa {self.alin_kerros}")
-        else:
-            print("Hissi on jo alimmassa kerroksessa.")
-
     # Metodi hissin siirtämiseksi haluttuun kerrokseen (functio)
     def siirry_kerrokseen(self, kerros):
         if kerros < self.alin_kerros:
@@ -34,6 +18,24 @@ class Hissi:
         elif kerros > self.alin_kerros:
             while kerros > self.alin_kerros:
                 self.kerros_ylos()
+
+        print(f"# Hissi on kerroksessa {self.alin_kerros} #")
+
+    # Metodi hissin siirtämiseksi ylöspäin (functio)
+    def kerros_ylos(self):
+        if self.alin_kerros < self.ylin_kerros:
+            self.alin_kerros += 1
+            print(f"Hissi on kerroksessa {self.alin_kerros}")
+        else:
+            print(f"Hissi on jo ylimmässä kerroksessa.")
+
+    # Metodi hissin siirtämiseksi alaspäin (functio)
+    def kerros_alas(self):
+        if self.alin_kerros > 1:
+            self.alin_kerros -= 1
+            print(f"Hissi on kerroksessa {self.alin_kerros}")
+        else:
+            print(f"Hissi on jo alimmassa kerroksessa.")
 
 
 # Luodaan Talo-luokka
@@ -48,18 +50,26 @@ class Talo:
         # Luodaan hissit
         for i in range(hissien_lkm):
             self.hissit.append(Hissi(alin_kerros, ylin_kerros))
+            # print(f"Luotiin hissi numero {i + 1} kerroksiin {alin_kerros} - {ylin_kerros}")
 
     # Metodi hissin ajamiseksi (functio)
-    def kayta_hissia(self, hissi_by_indeksi, kerros):
-        self.hissit[hissi_by_indeksi].siirry_kerrokseen(kerros)
+    def kayta_hissia(self, hissi_numero, kohdekerros):
+        if 0 < hissi_numero <= self.hissien_lkm:
+            hissi = self.hissit[hissi_numero - 1]  # Hissi-numerot / indexit alkavat nollasta
+            hissi.siirry_kerrokseen(kohdekerros)
+        else:
+            print(f"Hissi numero {hissi_numero} ei ole olemassa.")
 
 
 # Pääohjelma
-# Luodaan Talo-olio (object) # Ei ole oikea?
+# Luodaan Talo-olio (object)
 talo1 = Talo(1, 7, 3)
-print(talo1)
-talo1.kayta_hissia(0, 5)
-print(talo1)
-talo1.kayta_hissia(1, 3)
-print(talo1.hissit)
-talo1.kayta_hissia(2, 7)
+talo1.kayta_hissia(1, 5)
+print()
+talo1.kayta_hissia(2, 3)
+print()
+talo1.kayta_hissia(3, 7)
+print()
+
+for mika_hissi in talo1.hissit:
+    print(f"Hissi on kerroksessa {mika_hissi.alin_kerros}")
