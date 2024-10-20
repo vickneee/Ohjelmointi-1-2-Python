9c7d9760b31fa6f223fba5d4cceba6b0cff3d7e7
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2024-10-20 14:46:51 +0300

    Mod12 Teht1 commit

--- mod-12/Mod12-Teht1.py
+++ mod-12/Mod12-Teht1.py
@@ -1,16 +1,26 @@
 """Kirjoita ohjelma, joka hakee ja tulostaa satunnaisen Chuck Norris -vitsin käyttäjälle. Käytä seuravalla sivulla
 esiteltävää rajapintaa: https://api.chucknorris.io/. Käyttäjälle on näytettävä pelkkä vitsin teksti."""
 
+# Importoidaan tarvittavat kirjastot.
 import requests
 # import json
 
+# Chuck Norris API.
 url = "https://api.chucknorris.io/jokes/random"
 
+# Kokeillaan hakea Chuck Norris -vitsi.
 try:
+    # Kysely, jolla haetaan satunnainen vitsi. Parametrit: ei ole.
     response = requests.get(url)
+    # Tiedot saadaan JSON-muodossa.
     data = response.json()
+
+    # Tulostetaan JSON-muotoiset tiedot.
     # print(json.dumps(data, indent=2))
-    # print(data["id"])
+
+    # Tulostetaan vitsi.
     print(data["value"])
+
+# Virheenkäsittely
 except requests.exceptions.RequestException as e:
     print("Hakua ei voitu suorittaa.")
