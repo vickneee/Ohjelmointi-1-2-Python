c89fa1b92444bfa1366ec4fc92db0f10db66ab32
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2024-10-20 13:42:18 +0300

    Mod12 Teht1 commit

--- mod-12/Mod12-Teht1.py
+++ mod-12/Mod12-Teht1.py
@@ -2,12 +2,15 @@
 esiteltävää rajapintaa: https://api.chucknorris.io/. Käyttäjälle on näytettävä pelkkä vitsin teksti."""
 
 import requests
+import json
 
 url = "https://api.chucknorris.io/jokes/random"
 
 try:
     response = requests.get(url)
     data = response.json()
+    # print(json.dumps(data, indent=2))
+    # print(data["id"])
     print(data["value"])
 except requests.exceptions.RequestException as e:
     print("Hakua ei voitu suorittaa.")
