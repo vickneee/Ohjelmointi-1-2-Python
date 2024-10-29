ab78de6ae92f1c58454a2c849c832740cee5e0c4
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2024-10-29 19:40:12 +0200

    Tehtava 1

---
+++ harjoittelu/Tehtava-01.py
@@ -0,0 +1,37 @@
+class Tili:
+
+    lkm = 0
+
+    def __init__(self, omistaja, saldo=0):
+        self.omistaja = omistaja
+        self.saldo = saldo
+        Tili.lkm += 1
+        print(f"Tilejä luotu: {Tili.lkm}")
+
+    def maksa(self, amount):
+        if self.saldo >= amount:
+            self.saldo -= amount
+            print("Maksu onnistui")
+        else:
+            print("Ei tarpeeksi rahaa")
+        return
+
+    def tulostus(self):
+        print(f"Omistaja: {self.omistaja}, tilillä rahaa {self.saldo}")
+
+
+# Pääohjelma
+print("--- tilien luonti ---")
+t1 = Tili("Jorma")
+t2 = Tili("Anne", 100)
+
+print("--- maksut ---")
+t1.maksa(25)
+t2.maksa(25)
+
+print("--- tilien saldot ---")
+t1.tulostus()
+t2.tulostus()
+
+t2.maksa(25)
+t2.tulostus()
