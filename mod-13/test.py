a367178aa8e80453e665628ed0f518436a487fe9
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2024-10-29 17:15:29 +0200

    Test file added

---
+++ mod-13/test.py
@@ -0,0 +1,26 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/summa')
+def summa():
+    try:
+        args = request.args
+        luku1 = float(args.get("luku1"))
+        luku2 = float(args.get("luku2"))
+        summa = luku1+luku2
+
+        vastaus = {
+            "luku1": luku1,
+            "luku2": luku2,
+            "summa": summa
+        }
+
+        return jsonify(vastaus)
+    except ValueError:
+        return jsonify({"error": "Virheellinen yhteenlaskettava"}), 400
+
+
+if __name__ == '__main__':
+    app.run(use_reloader=True, host='127.0.0.1', port=3000)
