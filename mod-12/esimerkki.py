ce7672a93198a6e0614f1bf6bf1828d71a032a1e
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2024-10-29 14:08:54 +0200

    Status code 200 added

--- mod-12/esimerkki.py
+++ mod-12/esimerkki.py
@@ -1,15 +1,21 @@
 import requests
-import json
+# import json
 
 hakusana = input("Anna hakusana: ")
 
 try:
     response = requests.get("https://api.tvmaze.com/search/shows?q=" + hakusana)
-    data = response.json()
-    # print(json.dumps(data, indent=2))
-    # print(data)
-    for sarja in data:
-        print(sarja["show"]["name"])
+
+    if response.status_code == 200:
+        data = response.json()
+        # print(json.dumps(data, indent=2))
+        # print(data)
+
+        for sarja in data:
+            print(f"Name: {sarja["show"]["name"]}")
+            # print(f"Language: {sarja["show"]["language"]}")
+    else:
+        print("Hakusanalla ei löytynyt sarjoja.")
 
 except requests.exceptions.RequestException as e:
     print("Hakua ei voitu suorittaa.")
